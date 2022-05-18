8cbf1d1732cd81de0cc07b4d04883809f7dd8c69
Ricardo Pereira <pereira.rjg@gmail.com>
2022-05-18 14:06:08 +0100

    fixing typo

--- packages/health/lib/src/ecg_data.dart
+++ packages/health/lib/src/ecg_data.dart
@@ -5,14 +5,14 @@ class ECGData {
   double? period;
   int? interpretation;
   List<String>? symptoms;
-  double? averageHearthRate;
+  double? averageHeartRate;
 
   ECGData._({
     this.period,
     this.values,
     this.interpretation,
     this.symptoms,
-    this.averageHearthRate,
+    this.averageHeartRate,
   });
 
   factory ECGData.fromRawJson(String str) => ECGData.fromJson(json.decode(str));
@@ -25,7 +25,7 @@ class ECGData {
             json['values'] == null ? null : List<ECGValue>.from(json['values'].map((x) => ECGValue.fromJson(Map<String, dynamic>.from(x)))),
         interpretation: json['interpretation'] == null ? null : json['interpretation'],
         symptoms: json['symptoms'] == null ? null : List<String>.from(json['symptoms'].map((x) => x)),
-        averageHearthRate: json['average_heart_rate'] == null ? null : json['average_heart_rate'],
+        averageHeartRate: json['average_heart_rate'] == null ? null : json['average_heart_rate'],
       );
 
   Map<String, dynamic> toJson() => {
@@ -33,13 +33,13 @@ class ECGData {
         'values': values == null ? null : List<dynamic>.from(values!.map((x) => x.toJson())),
         'interpretation': interpretation == null ? null : interpretation,
         'symptoms': symptoms == null ? null : List<dynamic>.from(values!.map((x) => x.toJson())),
-        'average_heart_rate': averageHearthRate == null ? null : averageHearthRate,
+        'average_heart_rate': averageHeartRate == null ? null : averageHeartRate,
       };
 
   String toString() => '{values: $values, '
       'period: $period,'
       'interpretation: $interpretation '
-      'averageHearthRate: $averageHearthRate '
+      'averageHearthRate: $averageHeartRate '
       'symptoms: $symptoms}';
 
   @override
@@ -47,7 +47,7 @@ class ECGData {
     return o is ECGData &&
         this.interpretation == o.interpretation &&
         this.period == o.period &&
-        this.averageHearthRate == o.averageHearthRate &&
+        this.averageHeartRate == o.averageHeartRate &&
         ListEquality().equals(o.values, values) &&
         ListEquality().equals(o.symptoms, symptoms);
   }
